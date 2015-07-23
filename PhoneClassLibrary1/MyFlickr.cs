7546778ba7b984537f756e8be3092410c8df53cd
Jamie Kitson <jamie@kitten-x.com>
2015-07-23 23:40:02 +0100

    Revert to sha1, use "file:sha1sig"

--- PhoneClassLibrary1/MyFlickr.cs
+++ PhoneClassLibrary1/MyFlickr.cs
@@ -75,9 +75,14 @@ namespace PhoneClassLibrary1
                 {
                     Settings.DebugLog("Found picture " + p.Name);
 
+                    /*
                     MD5Managed hash = new MD5Managed();
                     hash.ComputeHash(p.GetImage());
                     string hashTag = "file:md5sum=" + BitConverter.ToString(hash.Hash).Replace("-", string.Empty);
+                    */
+                    SHA1Managed hash = new SHA1Managed();
+                    hash.ComputeHash(p.GetImage());
+                    string hashTag = "file:sha1sig=" + BitConverter.ToString(hash.Hash).Replace("-", "");
                     string filenameTag = "file:name=" + p.Name;
                     PhotoSearchOptions so = new PhotoSearchOptions("me", hashTag);
                     flickrReturned = false;
